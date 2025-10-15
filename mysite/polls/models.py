ffad585c1cb0a65511a92fbbdc59508017c4fd1b
Er1ckR1ck0 <eryckov37@gmail.com>
2025-10-15 07:41:31 +0300

    Обновлены модели и представления для автоматической установки даты публикации статьи. Упрощен шаблон формы.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,7 +4,7 @@ class Article(models.Model):
     title = models.CharField(max_length=120)
     author = models.CharField(max_length=120)
     content = models.TextField()
-    data_published = models.DateTimeField()
+    data_published = models.DateTimeField(auto_now_add=True) 
 
     # def __str__(self):
     #     return self.title
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,16 +11,13 @@ def contact_view(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
         if form.is_valid():
-            article = form.save(commit=False)
-            article.data_published = timezone.now()
-            article.save()
+            form.save()  # дата установится автоматически
             return redirect("/polls/success")
         else:
             print(form.errors)
     else:
         form = ArticleForm()
     return render(request, 'public.html', {'form': form })
-
 def success_view(request):
     return HttpResponse("Success!")
 
